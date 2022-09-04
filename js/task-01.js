ae33b012450acbcd410d3baa05459b8190e4df91
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-04 21:09:38 +0400

    Задание 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,10 @@
-console.log(1);
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
+
+for (let key of Object.keys(user)) {
+  console.log(`${key}: ${user[key]}`);
+}
