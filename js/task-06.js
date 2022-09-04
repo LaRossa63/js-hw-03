cd3466ef08e9f3f2772f07eb660cb1c9f57e9d5b
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-04 21:38:37 +0400

    Задание 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,1 +1,22 @@
-console.log(6);
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const calculateTotalPrice = (allProducts, productName) => {
+  let result = 0;
+
+  allProducts.forEach((element) => {
+    if (element.name === productName) {
+      result = element.price * element.quantity;
+    }
+  });
+
+  return result;
+};
+
+console.log(calculateTotalPrice(products, 'Радар')); // 5200
+
+console.log(calculateTotalPrice(products, 'Дроид')); // 2800
