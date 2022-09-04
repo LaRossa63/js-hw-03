49e2d06e8a4defe5b878ba2587603f29757b1fe6
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-04 21:31:15 +0400

    Задание 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,1 +1,24 @@
-console.log(5);
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = (arr, prop) => {
+  const result = [];
+
+  arr.forEach((element) => {
+    if (element[prop]) {
+      result.push(element[prop]);
+    }
+  });
+
+  return result;
+};
+
+console.log(getAllPropValues(products, 'name'));
+
+console.log(getAllPropValues(products, 'quantity'));
+
+console.log(getAllPropValues(products, 'category'));
