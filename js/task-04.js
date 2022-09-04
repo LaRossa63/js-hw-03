3cdbd87f04ee570a8b1ae0e483315a0518bd5958
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-04 21:23:53 +0400

    Задание 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,26 @@
-console.log(4);
+const countTotalSalary = (employees) => {
+  const total = Object.values(employees).reduce(
+    (total, element) => total + element,
+    0
+  );
+
+  return `Общая заплата: ${total}`;
+};
+
+console.log(countTotalSalary({}));
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  })
+);
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  })
+);
