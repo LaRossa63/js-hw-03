6b8f6cbc129d740cca9cdc772b34b1d23102001f
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-04 21:56:36 +0400

    Задание 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,1 +1,97 @@
-console.log(7);
+/*
+ * Типов транзацкий всего два.
+ * Можно положить либо снять деньги со счета.
+ */
+const Transaction = {
+  DEPOSIT: 'deposit',
+  WITHDRAW: 'withdraw',
+};
+
+/*
+ * Каждая транзакция это объект со свойствами: id, type и amount
+ */
+
+const account = {
+  // Текущий баланс счета
+  balance: 100,
+
+  // История транзакций
+  transactions: [],
+
+  /*
+   * Метод создает и возвращает объект транзакции.
+   * Принимает сумму и тип транзакции.
+   */
+  createTransaction(amount, type) {
+    return { id: this.transactions.length + 1, type, amount };
+  },
+
+  /*
+   * Метод отвечающий за добавление суммы к балансу.
+   * Принимает сумму танзакции.
+   * Вызывает createTransaction для создания объекта транзакции
+   * после чего добавляет его в историю транзакций
+   */
+  deposit(amount) {
+    const transaction = this.createTransaction(amount, Transaction.DEPOSIT);
+    this.transactions.push(transaction);
+    this.balance += amount;
+  },
+
+  /*
+   * Метод отвечающий за снятие суммы с баланса.
+   * Принимает сумму танзакции.
+   * Вызывает createTransaction для создания объекта транзакции
+   * после чего добавляет его в историю транзакций.
+   *
+   * Если amount больше чем текущий баланс, выводи сообщение
+   * о том, что снятие такой суммы не возможно, недостаточно средств.
+   */
+  withdraw(amount) {
+    const transaction = this.createTransaction(amount, Transaction.WITHDRAW);
+    this.transactions.push(transaction);
+
+    if (this.balance <= amount) {
+      console.log('Недостаточно средств.');
+      return;
+    }
+
+    this.balance -= amount;
+  },
+
+  /*
+   * Метод возвращает текущий баланс
+   */
+  getBalance() {
+    return this.balance;
+  },
+
+  /*
+   * Метод ищет и возвращает объект транзаций по type
+   */
+  getTransactionDetails(type) {
+    return this.transactions.filter((element) => element.type === type);
+  },
+
+  /*
+   * Метод возвращает количество средств
+   * определенного типа транзакции из всей истории транзакций
+   */
+  getTransactionTotal(type) {
+    console.log(`Баланс: ${this.getBalance()}\n`);
+    console.log(`Все транзакции с типом: ${type}`);
+
+    const transactions = this.getTransactionDetails(type);
+
+    if (!transactions.length) {
+      console.log('Транзакции не найдены!');
+      return;
+    }
+
+    transactions.forEach((element) => {
+      console.log(
+        `[${element.id}] тип ${element.type} сумма: ${element.amount}`
+      );
+    });
+  },
+};
